ba86c518371166748e41a9e9f41c37692e021342
Edwards-Alex <1032607679@gmail.com>
2025-08-26 09:56:23 +0800

    completed trailversSection

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import FeatureSection from '../components/FeatureSection'
 import HeroSection from '../components/HeroSection'
+import TrailersSection from '../components/TrailersSection'
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
       <FeatureSection />
+      <TrailersSection />
     </div>
   )
 }
